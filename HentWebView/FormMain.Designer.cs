de39d3ac27279079a2d299028709d67085bafd48
James Lin <act70255@gmail.com>
2023-01-29 18:37:12 +0800

    rename form

--- HentWebView/FormMain.Designer.cs
+++ HentWebView/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HentWebView
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         ///  Required designer variable.
--- HentWebView/FormMain.cs
+++ HentWebView/FormMain.cs
@@ -3,9 +3,9 @@ using System.Windows.Forms;
 
 namespace HentWebView
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
             InitializeList();
